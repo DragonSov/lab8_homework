0c521ef1eb43bb337d6d16fc50c5fb0ca2f57918
DragonSov <bandicam861@gmail.com>
2025-11-18 01:01:57 +0300

    Добавьте файлы проекта.

---
+++ main.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_CTYPE, "RUS");
+
+    int N;
+    double sum = 0;
+
+    printf("Введите N: ");
+    scanf("%d", &N);
+
+    for (int i = N; i <= 2 * N; i++) {
+        sum += i * i;
+    }
+
+    printf("Сумма квадратов от N до 2N: %.2f\n", sum);
+
+    return 0;
+}
